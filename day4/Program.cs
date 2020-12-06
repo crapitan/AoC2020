69c4d401583b4261ed99702fe6d4e21630e18fdd
Klas Broberg <klas.broberg@gmail.com>
2020-12-06 22:34:22 +0100

    Partial day 4 (needs validation) and Solution day 5

---
+++ day4/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText(@"D:\Crap\adventofcode2020\day4\data.txt");
+            var rows = SplitOnEmptyLine(data).ToList();
+
+            var passPorts = rows.Select(r => new Passport(r));
+
+            var validPassPorts = passPorts.Where(p => p.IsValid).Count();
+
+            System.Console.WriteLine($"Of {passPorts.Count()} passports {validPassPorts} is valid");
+        }
+
+
+        public static IEnumerable<string> SplitOnEmptyLine(string data)
+        {
+            var rows = data.Split(Environment.NewLine);
+            List<string> myData = new List<string>();
+
+            string response = string.Empty;
+
+            foreach (var row in rows)
+            {
+                if (string.IsNullOrEmpty(row))
+                {
+                    myData.Add(response + "");
+                    response = string.Empty;
+                }
+                else
+                {
+                    response = response + " " + row;
+                }
+            }
+
+            myData.Add(response + "");
+            return myData;
+        }
+    }
+
+    public class Passport
+    {
+        public Passport(string input)
+        {
+            var rows = input.Split(' ');
+
+            foreach (var item in rows)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+
+
+                    var type = item.Substring(0, 3);
+                    switch (type)
+                    {
+                        case "byr":
+                            this.BirthYear = item.Substring(4);
+                            break;
+                        case "iyr":
+                            this.IssuYear = item.Substring(4);
+                            break;
+                        case "eyr":
+                            this.ExpirationYear = item.Substring(4);
+                            break;
+                        case "hgt":
+                            this.Height = item.Substring(4);
+                            break;
+                        case "hcl":
+                            this.HairColor = item.Substring(4);
+                            break;
+                        case "ecl":
+                            this.EyeColor = item.Substring(4);
+                            break;
+                        case "pid":
+                            this.PassportId = item.Substring(4);
+                            break;
+                        case "cid":
+                            this.CountryId = item.Substring(4);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        public string BirthYear { get; set; }
+        public string IssuYear { get; set; }
+        public string ExpirationYear { get; set; }
+        public string Height { get; set; }
+        public string HairColor { get; set; }
+        public string EyeColor { get; set; }
+        public string PassportId { get; set; }
+        public string CountryId { get; set; }
+
+        public bool IsValid
+        {
+            get
+            {
+
+                if (string.IsNullOrEmpty(BirthYear) ||
+                    string.IsNullOrEmpty(IssuYear) ||
+                    string.IsNullOrEmpty(Height) ||
+                    string.IsNullOrEmpty(HairColor) ||
+                    string.IsNullOrEmpty(EyeColor) ||
+                    string.IsNullOrEmpty(PassportId) ||
+                    // string.IsNullOrEmpty(PassCountryIdportId) ||
+                    string.IsNullOrEmpty(ExpirationYear))
+                {
+                  //  System.Console.WriteLine("Not all fields where pressent!");
+                    return false;
+                }
+
+                if (!(this.BirthYear.Length == 4 && int.TryParse(this.BirthYear, out int year) && year >= 1920 && year <= 2002))
+                {
+                    System.Console.WriteLine($"The birthyear of {BirthYear} is not between 1920 and 2002");
+                    return false;
+                }
+                else
+                {
+                    System.Console.WriteLine($"The birthyear of {BirthYear} is between 1920 and 2002");
+                }
+
+                if (!(this.IssuYear.Length == 4 && int.TryParse(this.IssuYear, out int issueyear) && issueyear >= 2010 && issueyear <= 2020))
+                    return false;
+
+                if (!(this.ExpirationYear.Length == 4 && int.TryParse(this.ExpirationYear, out int expyear) && expyear >= 2020 && expyear <= 2030))
+                    return false;
+
+                if (this.Height.Contains("cm"))
+                {
+                    if (!(int.TryParse(this.Height.Replace("cm", string.Empty), out int heightcm) && heightcm >= 150 && heightcm <= 193))
+                        return false;
+                }
+
+                if (this.Height.Contains("in"))
+                {
+                    if (!(int.TryParse(this.Height.Replace("in", string.Empty), out int heightin) && heightin >= 59 && heightin <= 76))
+                        return false;
+                }
+
+                if (this.HairColor.Length != 7)
+                {
+                    return false;
+                }
+
+                if (!Regex.IsMatch(this.HairColor, "^#[0-9a-f]{6}"))
+                {
+                    return false;
+                }
+
+                if (!(EyeColor == "amb" ||
+                        EyeColor == "blu" ||
+                        EyeColor == "brn" ||
+                        EyeColor == "gry" ||
+                        EyeColor == "grn" ||
+                        EyeColor == "hzl" ||
+                        EyeColor == "oth"))
+                {
+                    return false;
+                }
+
+                if (!Regex.IsMatch("000000000", "^[0-9]{9}$"))
+                {
+                    return false;
+                }
+
+
+
+                if (!Regex.IsMatch(this.PassportId, "^[0-9]{9}$"))
+                {
+                    return false;
+                }
+
+                return true;
+                ;
+
+            }
+        }
+    }
+}
---
+++ day5/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace day5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var data = File.ReadAllText(@"D:\Crap\adventofcode2020\day5\data.txt");
+
+            // Test
+            var seating = new Seating("FBFBBFFRLR");
+            System.Console.WriteLine($" Row {seating.Row} Seating {seating.Seat} : Id {seating.Id}");
+
+            var seats = data.Split(Environment.NewLine).Select(l => new Seating(l)).OrderBy(b => b.Id).ToArray();
+
+            var maxSeatId = seats.Max(s => s.Id);
+            System.Console.WriteLine($"Max seat Id is {maxSeatId}");
+
+            for (var i = 1; i <= seats.Length-1; i++)
+            {
+                 if (seats[i-1].Id < seats[i].Id -1)
+                     System.Console.WriteLine($"{seats[i].Id} is your seat");
+             }
+        }
+    }
+
+    public class Seating
+    {
+        public Seating(string input)
+        {
+            Row = CalculateRow(input, 0, 127);
+            Seat = CalculateSeat(input, 0, 7);
+            Id = Row * 8 + Seat;
+        }
+
+        public int Id { get; set; }
+        public int Row { get; set; }
+
+        public int Seat { get; set; }
+
+        private int CalculateRow(string intput, int from, int to)
+        {
+            if (from == to) return from;
+
+            string firstLetter = intput.First() + "";
+
+            intput = intput.Remove(0, 1);
+
+            if (firstLetter == "L" || firstLetter == "R") return from;
+
+            if (firstLetter == "F")
+            {
+                int newRowSet = (int)Math.Floor( to - ((to - from) / 2d));
+                System.Console.WriteLine($"Row is between {from} and {newRowSet}");
+                return CalculateRow(intput, from, newRowSet);
+            }
+            else
+            {
+                 int newRowSet = (int)Math.Ceiling(from + ( (to - from) / 2d));
+                 System.Console.WriteLine($"Row is between {newRowSet} to {to}");
+                return CalculateRow(intput, newRowSet, to);
+            }
+        }
+
+         private int CalculateSeat(string intput, int from, int to)
+        {
+            if (from == to) return from;
+
+            string firstLetter = intput.First() + "";
+            intput = intput.Remove(0, 1);
+
+            if (firstLetter == "F" || firstLetter == "B") return CalculateSeat(intput, from, to);
+
+            if (firstLetter == "L")
+            {
+                int newRowSet = (int)Math.Floor( to - ((to - from) / 2d));
+                System.Console.WriteLine($"Seat is between {from} and {newRowSet}");
+                return CalculateSeat(intput, from, newRowSet);
+            }
+            else
+            {
+                 int newRowSet = (int)Math.Ceiling(from + ( (to - from) / 2d));
+                 System.Console.WriteLine($"Seat is between {newRowSet} to {to}");
+                return CalculateSeat(intput, newRowSet, to);
+            }
+        }
+    }
+
+
+
+
+}
